b74734d3e26f799baee0f166b20dec3d2a8ec723
reinaka <juliabednaia@gmail.com>
2024-11-25 15:57:31 +0300

    module_1(fix): add violet button styles

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -5,12 +5,19 @@ type ButtonPropsT = {
   radius: 8 | 16 | 20;
   disabled?: boolean;
   type?: "button" | "submit" | "reset";
+  color?: "blue" | "violet";
 };
 
-export const Button = ({ children, radius, disabled, type }: ButtonPropsT) => {
+export const Button = ({
+  children,
+  radius,
+  disabled,
+  type,
+  color,
+}: ButtonPropsT) => {
   return (
     <button
-      className={`${styles.button} ${styles[`button--${radius}`]}`}
+      className={`${styles.button} ${styles[`button--${radius}`]} ${color === "violet" ? styles["button--violet"] : styles[`button--blue`]}`}
       disabled={disabled}
       type={type || "button"}
     >
