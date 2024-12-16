b74a97eaa10f848e40ad5c213f5e1c1cc713ba24
reinaka <juliabednaia@gmail.com>
2024-12-16 16:12:05 +0300

    module_2(fix): import fix

--- src/components/home-page-blocks/subscription/subscription-form/subscription-form.tsx
+++ src/components/home-page-blocks/subscription/subscription-form/subscription-form.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Button } from "@ui/button/button";
+import { Button } from "@ui/button";
 import styles from "./subscription-form.module.scss";
 
 export const SubscriptionForm = () => {
