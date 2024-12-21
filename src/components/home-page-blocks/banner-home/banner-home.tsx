0257f3ff9de4719d708288d0ff541547dadcf77e
reinaka <juliabednaia@gmail.com>
2024-12-21 22:11:01 +0300

    module_3(refactor): rename Banner to BannerHome

--- src/components/home-page-blocks/banner-home/banner-home.tsx
+++ src/components/home-page-blocks/banner-home/banner-home.tsx
@@ -1,27 +1,27 @@
 import { Button } from "@ui/button";
 import { CreditCard } from "@ui/credit-card";
-import styles from "./banner.module.scss";
+import styles from "./banner-home.module.scss";
 
-const bannerCards = [
+const bannerHomeCards = [
   {
     title: "credit card 1",
-    src: "/banner-cards/card1.jpg",
+    src: "/credit-cards/card1.jpg",
   },
   {
     title: "credit card 2",
-    src: "/banner-cards/card2.jpg",
+    src: "/credit-cards/card2.jpg",
   },
   {
     title: "credit card 3",
-    src: "/banner-cards/card3.jpg",
+    src: "/credit-cards/card3.jpg",
   },
   {
     title: "credit card 4",
-    src: "/banner-cards/card4.jpg",
+    src: "/credit-cards/card4.jpg",
   },
 ];
 
-export const Banner = () => {
+export const BannerHome = () => {
   return (
     <section role="banner" className={styles.banner__wrapper}>
       <div className={styles.banner__applyBlock}>
@@ -31,7 +31,7 @@ export const Banner = () => {
         <Button radius={16}>Choose the card</Button>
       </div>
       <ul className={styles.card__wrapper}>
-        {bannerCards.map(card => (
+        {bannerHomeCards.map(card => (
           <li key={card.src} className={styles.card__item}>
             <CreditCard card={card} additionalStyles={styles.card}/>
           </li>
--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -1,4 +1,4 @@
-import { Banner } from "@components/home-page-blocks/banner";
+import { BannerHome} from "@components/home-page-blocks/banner-home";
 import { ExchangeRates } from "@components/home-page-blocks/exchange-rates";
 import { Features } from "@components/home-page-blocks/features";
 import { Locations } from "@components/home-page-blocks/locations";
@@ -9,7 +9,7 @@ import styles from "./home-page.module.scss";
 export const HomePage = () => {
   return (
     <div className={styles.home__wrapper}>
-      <Banner />
+      <BannerHome />
       <Features />
       <ExchangeRates />
       <Locations />
