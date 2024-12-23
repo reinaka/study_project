9557e247f0d5c39b4183ebec6883607a402d0010
reinaka <juliabednaia@gmail.com>
2024-12-23 21:16:19 +0300

    module_3(refactor): refactor home page imports

--- src/pages/home-page/home-page.tsx
+++ src/pages/home-page/home-page.tsx
@@ -1,9 +1,11 @@
-import { BannerHome } from "@components/home-page-blocks/banner-home";
-import { ExchangeRates } from "@components/home-page-blocks/exchange-rates";
-import { Features } from "@components/home-page-blocks/features";
-import { Locations } from "@components/home-page-blocks/locations";
-import { News } from "@components/home-page-blocks/news";
-import { Subscription } from "@components/home-page-blocks/subscription";
+import {
+  BannerHome,
+  Features,
+  ExchangeRates,
+  Locations,
+  News,
+  Subscription,
+} from "@components/home-page-blocks";
 import styles from "./home-page.module.scss";
 
 export const HomePage = () => {
