38a5e2edd1013d0b881f07f0bcc7ff58a1e12b21
reinaka <juliabednaia@gmail.com>
2025-01-09 02:28:23 +0300

    module_3: add comments to getFilteredNews func

--- src/utils/functions/news/getFilteredNews.ts
+++ src/utils/functions/news/getFilteredNews.ts
@@ -5,11 +5,17 @@ export const getFilteredArticles = async (
   articles: NewsItemT[] | [],
   limit: number,
 ) => {
+  // рекурсия:
+  // проверяем сначала количество статей, равное лимиту (необходимому количеству),
+  // потом дополнительно проверяем еще статьи, если не все прошли проверку на предыдущем этапе
   async function filterArticles(
     resultArr: FilteredNewsItemT[],
     start: number,
     end: number | undefined,
   ) {
+    // проверяем, достаточно ли еще статей для проверки:
+    // если нет, то проверяем столько, сколько есть, и возвращаем результат
+    // если да и лимит не заполнен, то вызываем рекурсивную функцию снова
     if (end != undefined && articles.length <= end) end = undefined;
     const articlesSlice = articles.slice(start, end);
     const promises = articlesSlice.map(article => checkArticle(article));
@@ -29,6 +35,8 @@ export const getFilteredArticles = async (
     } else return resultArr;
   }
 
+  // результат:
+  // получаем либо количество, равное лимиту, либо меньшее количество (если валидных статей было недостаточно)
   const filteredArticlesResult = await filterArticles([], 0, limit + 1);
   return filteredArticlesResult;
 };
