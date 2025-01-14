09efed521533498d9d5cf139620bfcaeacd70b78
reinaka <juliabednaia@gmail.com>
2025-01-14 11:26:06 +0300

    module_4(feat): add transparent background for form

--- src/components/page-blocks/loan-page-blocks/form/form.tsx
+++ src/components/page-blocks/loan-page-blocks/form/form.tsx
@@ -1,14 +1,24 @@
 import { ReactNode } from "react";
+import classNames from "classnames/bind";
 import styles from "./form.module.scss";
 
 export type FormPropsT = {
   children: ReactNode;
   submitHandler: React.FormEventHandler<HTMLFormElement>;
+  additionalStyles?: string;
+  background?: "transparent" | "background";
 };
 
-export const Form = ({ children, submitHandler }: FormPropsT) => {
+export const Form = ({ children, submitHandler, additionalStyles, background }: FormPropsT) => {
+  const cx = classNames.bind(styles);
+  const formStyles = cx({
+    form__wrapper: true,
+    [`form--${background ? "transparent" : "background"}`]: true,
+    [`${additionalStyles}`]: additionalStyles,
+  })
+
   return (
-    <form onSubmit={submitHandler} className={styles.form__wrapper} noValidate>
+    <form onSubmit={submitHandler} className={formStyles} noValidate>
       {children}
     </form>
   );
