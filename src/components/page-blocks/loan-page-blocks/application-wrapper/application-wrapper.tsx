721cf51eb07f62c4abf8d6f2d8cf39a120d671ee
reinaka <juliabednaia@gmail.com>
2025-01-14 11:30:20 +0300

    module_4(feat): add application wrapper

---
+++ src/components/page-blocks/loan-page-blocks/application-wrapper/application-wrapper.tsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import { Loader, Notification } from "@components/ui";
+import { ScoringForm } from "../form/components";
+import { PaymentSchedule } from "../payment-schedule";
+import { DocumentsSign } from "../documents-sign";
+import { ConfirmationCode } from "../confirmation-code";
+import styles from "./application-wrapper.module.scss";
+
+export type ApplicationWrapperPropsT = {
+  elem:
+    | "SCORING_FORM"
+    | "PAYMENT_SCHEDULE"
+    | "DOCUMENTS_SIGN"
+    | "CONFIRMATION_CODE";
+  notificationTitle: string;
+  notificationText: string;
+};
+
+export type ApplicationWrapperStateSettersT = {
+  setIsLoading: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsError: React.Dispatch<React.SetStateAction<boolean>>;
+  setIsSuccess: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+export const ApplicationWrapper = ({
+  elem,
+  notificationTitle,
+  notificationText,
+}: ApplicationWrapperPropsT) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  const content = (elem => {
+    if (elem === "SCORING_FORM")
+      return (
+        <ScoringForm
+          setIsLoading={setIsLoading}
+          setIsError={setIsError}
+          setIsSuccess={setIsSuccess}
+        />
+      );
+    else if (elem === "PAYMENT_SCHEDULE")
+      return (
+        <PaymentSchedule
+          setIsLoading={setIsLoading}
+          setIsError={setIsError}
+          setIsSuccess={setIsSuccess}
+        />
+      );
+    else if (elem === "DOCUMENTS_SIGN")
+      return (
+        <DocumentsSign
+          setIsLoading={setIsLoading}
+          setIsError={setIsError}
+          setIsSuccess={setIsSuccess}
+        />
+      );
+    else if (elem === "CONFIRMATION_CODE")
+      return (
+        <ConfirmationCode
+          setIsLoading={setIsLoading}
+          setIsError={setIsError}
+          setIsSuccess={setIsSuccess}
+          length={4}
+        />
+      );
+  })(elem);
+
+  return (
+    <div className={styles.page__wrapper}>
+      {isLoading ? (
+        <Loader />
+      ) : isError ? (
+        <Notification>Something went wrong, try again later</Notification>
+      ) : isSuccess ? (
+        <Notification
+          title={notificationTitle}
+          additionalStyles={styles.notification}
+        >
+          {notificationText}
+        </Notification>
+      ) : (
+        content
+      )}
+    </div>
+  );
+};
